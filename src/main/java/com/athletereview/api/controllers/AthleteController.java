c487e836a84dd6e4ee94267de3c3e4dee2feb6ea
Kenzysc <kenzysc@gmail.com>
2023-09-05 00:31:31 +0100

    Added function parameters for AthelteController

--- src/main/java/com/athletereview/api/controllers/AthleteController.java
+++ src/main/java/com/athletereview/api/controllers/AthleteController.java
@@ -4,9 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,28 +21,29 @@ import com.athletereview.api.dto.AthleteResponseDto;
 public class AthleteController {
 	
 	@GetMapping("athlete")
-	public ResponseEntity<AthleteResponseDto> getAthletes() {
+	public ResponseEntity<AthleteResponseDto> getAthletes(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+														  @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize) {
 		return null;
 	}
 	
 	@GetMapping("athlete/{id}")
-	public ResponseEntity<AthleteDto> athleteDetail() {
+	public ResponseEntity<AthleteDto> athleteDetail(@PathVariable int id) {
 		return null;
 	}
 	
 	@PostMapping("athlete/create")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<AthleteDto> createAthlete() {
+	public ResponseEntity<AthleteDto> createAthlete(@RequestBody AthleteDto athleteDto) {
 		return null;
 	}
 	
 	@PutMapping("athlete/{id}/update")
-	public  ResponseEntity<AthleteDto> updateAthlete() {
+	public  ResponseEntity<AthleteDto> updateAthlete(@PathVariable("id") int pokemonId, @RequestBody AthleteDto athlete ) {
 		return null;
 	}
 	
 	@DeleteMapping("athlete/{id}/delete")
-	public ResponseEntity<String> deleteAthlete() {
+	public ResponseEntity<String> deleteAthlete(@PathVariable("id") int pokemonId) {
 		return null;
 	}
 	
