bb5ac5e3823f622002bdc24d393938a59ac8e550
Kenzysc <kenzysc@gmail.com>
2023-09-06 18:22:02 +0100

    Add authresponse, login and register dto

---
+++ src/main/java/com/athletereview/api/dto/AuthResponseDto.java
@@ -0,0 +1,13 @@
+package com.athletereview.api.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDto {
+	private String accessToken;
+	private String tokenType = "Bearer ";
+	
+	public AuthResponseDto(String accessToken) {
+		this.accessToken = accessToken;
+	}
+}
---
+++ src/main/java/com/athletereview/api/dto/RegisterDto.java
@@ -0,0 +1,9 @@
+package com.athletereview.api.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+	private String username;
+	private String password;
+}
