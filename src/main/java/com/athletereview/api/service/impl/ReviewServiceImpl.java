3d5a06abef5fc851dea4ae7ec58c770c636223e4
Kenzysc <kenzysc@gmail.com>
2023-09-06 14:39:02 +0100

    Build out review service impl

--- src/main/java/com/athletereview/api/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/athletereview/api/service/impl/ReviewServiceImpl.java
@@ -1,11 +1,17 @@
 package com.athletereview.api.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.athletereview.api.dto.ReviewDto;
+import com.athletereview.api.exceptions.AthleteNotFoundException;
+import com.athletereview.api.exceptions.ReviewNotFoundException;
+import com.athletereview.api.models.Athlete;
+import com.athletereview.api.models.Review;
+import com.athletereview.api.repository.AthleteRepository;
 import com.athletereview.api.repository.ReviewRepository;
 import com.athletereview.api.service.ReviewService;
 
@@ -13,39 +19,97 @@ import com.athletereview.api.service.ReviewService;
 public class ReviewServiceImpl implements ReviewService {
 
 	private ReviewRepository reviewRepository;
+	private AthleteRepository athleteRepository;
 	
 	@Autowired
-	public ReviewServiceImpl(ReviewRepository reviewRepository) {
+	public ReviewServiceImpl(ReviewRepository reviewRepository, AthleteRepository athleteRepository) {
 		this.reviewRepository = reviewRepository;
 	}
 
 	@Override
 	public ReviewDto createReview(int athleteId, ReviewDto reviewDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Review review = mapToEntity(reviewDto);
+		
+		Athlete athlete = athleteRepository.findById(athleteId).orElseThrow(() -> new AthleteNotFoundException("Athlete with associated review not found"));
+		
+		review.setAthlete(athlete);
+		
+		Review newReview =  reviewRepository.save(review);
+		
+		return mapToDto(newReview);
+	}
+	
+	private ReviewDto mapToDto(Review review) {
+		ReviewDto reviewDto = new ReviewDto();
+		reviewDto.setId(review.getId());
+		reviewDto.setTitle(review.getTitle());
+		reviewDto.setContent(review.getContent());
+		reviewDto.setStars(review.getStars());
+		return reviewDto;
+	}
+	
+	private Review mapToEntity(ReviewDto reviewDto) {
+		Review review = new Review();
+		review.setTitle(reviewDto.getTitle());
+		review.setContent(reviewDto.getContent());
+		review.setStars(reviewDto.getStars());
+		return review;
 	}
 
 	@Override
 	public List<ReviewDto> getReviewByAthleteId(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Review> reviews = reviewRepository.findByAthleteId(id);
+		
+		return reviews.stream().map(review -> mapToDto(review)).collect(Collectors.toList());
 	}
 
 	@Override
 	public ReviewDto getReviewById(int reviewId, int athleteId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Athlete athlete = athleteRepository.findById(athleteId).orElseThrow(() -> new AthleteNotFoundException("Athlete with associated review not found"));
+		Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new ReviewNotFoundException("Review with associated pokemon not found"));
+		
+		// use a wrapper for equals() method instead of !=
+		if(review.getAthlete().getId() != athlete.getId()) { 
+			
+			throw new ReviewNotFoundException("this review does not belong to a athlete");
+		}
+		
+		return mapToDto(review);
 	}
 
 	@Override
 	public ReviewDto updateReview(int athleteId, int reviewId, ReviewDto reviewDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Athlete athlete = athleteRepository.findById(athleteId).orElseThrow(() -> new AthleteNotFoundException("Athlete with associated review not found"));
+		Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new ReviewNotFoundException("Review with associated pokemon not found"));
+		
+		// use a wrapper for equals() method instead of !=
+		if(review.getAthlete().getId() != athlete.getId()) { 
+			
+			throw new ReviewNotFoundException("this review does not belong to a athlete");
+		}
+		
+		review.setTitle(reviewDto.getTitle());
+		review.setContent(reviewDto.getContent());
+		review.setStars(reviewDto.getStars());
+		
+		Review updateReview = reviewRepository.save(review);
+		
+		return mapToDto(updateReview);
 	}
 
 	@Override
 	public void deleteReview(int athleteId, int reviewId) {
-		// TODO Auto-generated method stub
+		Athlete athlete = athleteRepository.findById(athleteId).orElseThrow(() -> new AthleteNotFoundException("Athlete with associated review not found"));
+		Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new ReviewNotFoundException("Review with associated pokemon not found"));
+		
+		// use a wrapper for equals() method instead of !=
+		if(review.getAthlete().getId() != athlete.getId()) { 
+			
+			throw new ReviewNotFoundException("this review does not belong to a athlete");
+		}
+		
+		reviewRepository.delete(review);
 		
 	}
 
