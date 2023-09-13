5ed6d128290ea74c3826603387bde88f43895e83
Kenzysc <kenzysc@gmail.com>
2023-09-13 01:00:37 +0100

    feat: add db seed to roles table on app init

--- src/main/java/com/athletereview/api/ApiApplication.java
+++ src/main/java/com/athletereview/api/ApiApplication.java
@@ -1,7 +1,15 @@
 package com.athletereview.api;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.athletereview.api.models.Role;
+import com.athletereview.api.repository.RoleRepository;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +17,16 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	
+	// adds USER and ADMIN roles to "roles" when spring app loads
+	@Bean
+    CommandLineRunner init (RoleRepository roleRepo){
+        return args -> {
+           List<String> roles = Arrays.asList("ADMIN", "USER");
+        	
+        	Role role = new Role();
+        	roles.forEach(name -> role.setName(name));
+        	roleRepo.save(role);
+        };
+    }
 }
--- src/main/java/com/athletereview/api/models/Role.java
+++ src/main/java/com/athletereview/api/models/Role.java
@@ -5,12 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "roles")
 public class Role {
