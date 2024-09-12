4f2eec7f1fa3c6b5b0f073d0bf4b921ff4fd2c2d
Kenzysc <kenzysc@gmail.com>
2024-09-12 18:53:00 +0100

    Modify the jwt expiration so the token expires in a week

--- src/main/java/com/athletereview/api/security/SecurityConstants.java
+++ src/main/java/com/athletereview/api/security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.athletereview.api.security;
 
 public class SecurityConstants {
-	public static final long JWT_EXPIRATION = 700000;
+	public static final long JWT_EXPIRATION = 604800000; // this value is 7days. 700000 is 11min in millisecond
 	public static final String JWT_SECRET = "secret";
 }
