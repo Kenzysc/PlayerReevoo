6bf282da21026d3ecac4e83cb42a2a7b00f1db36
Kenzysc <kenzysc@gmail.com>
2024-09-19 15:00:42 +0100

    feat: Add SwaggerConfig to enable bearer token

---
+++ src/main/java/com/athletereview/api/security/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.athletereview.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customApi() {
+        return new OpenAPI()
+            .info(new Info().title("PlayerReevoo API").version("v0.1")) 
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                    .addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")));
+    }
+}
